056e45c4ae3c1a988b43c93c93e193b036dd44dd
Juyeong Lee <juyeonglee0413@gmail.com>
2020-07-24 02:51:24 +0900

    whitespace

--- myblog/settings.py
+++ myblog/settings.py
@@ -135,5 +135,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-SITE_ID=1
+SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
